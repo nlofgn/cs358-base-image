dd901941276768294cd5fac8c0af4e4e11441830
Joe Hummel, PhD <joe.hummel@northwestern.edu>
2025-08-19 21:58:44 -0500

    new setup dir

---
+++ test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << endl;
+	cout << "It looks like docker image is successfully built and running!" << endl;
+	cout << endl;
+
+	return 0;
+}
